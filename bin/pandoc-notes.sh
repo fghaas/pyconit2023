fcff6aeea6bd4cdece9ff545b7fbc2724831894c
Florian Haas <xahteiwi@gmail.com>
2022-12-14 09:03:37 +0100

    Enable epub output
    Add a metadata file for pandoc, and update the pandoc convenience
    script to support epub output.

--- bin/pandoc-notes.sh
+++ bin/pandoc-notes.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-pandoc -t odt -o notes.odt markdown/00-intro.md markdown/talk.md
+FORMAT=$1
+
+if [ -z "$FORMAT" ]; then
+    echo "Usage: $0 FORMAT" 2>&1
+    exit 1
+fi
+
+pandoc --metadata-file=markdown/pandoc.yaml -t $FORMAT -o notes.${FORMAT} markdown/00-intro.md markdown/talk.md
